0c25847d7691d9286e0a1f5da6c45c2c105a883e
jmkimani <mainajkimani@gmail.com>
2023-04-14 22:59:56 +0300

    add more functionality

--- src/app/tools/resistance/page.tsx
+++ src/app/tools/resistance/page.tsx
@@ -1,48 +1,110 @@
+'use client'
 import { redirect } from "next/dist/server/api-utils"
+import { useState } from "react"
 
 interface ColorCodes {
-  name: string,
-  value: number | undefined,
-  tolerance: number | undefined
+  color: string,
+  value: number | null,
+  tolerance: number | null
 }
 
 const colorCodes: ColorCodes[] = [
-  { name: 'black', value: 0, tolerance: undefined },
-  { name: 'brown', value: 1, tolerance: 1 },
-  { name: 'red', value: 2, tolerance: 2 },
-  { name: 'orange', value: 3, tolerance: undefined },
-  { name: 'yellow', value: 4, tolerance: undefined },
-  { name: 'green', value: 5, tolerance: 0.5 },
-  { name: 'blue', value: 6, tolerance: 0.25 },
-  { name: 'violet', value: 7, tolerance: 0.1 },
-  { name: 'gray', value: 8, tolerance: 0.05 },
-  { name: 'white', value: 9, tolerance: undefined },
-  { name: 'gold', value: 0.1, tolerance: 5 },
-  { name: 'silver', value: 0.01, tolerance: 10 },
-  { name: '', value: undefined, tolerance: 20 },
+  { color: 'black', value: 0, tolerance: null },
+  { color: 'brown', value: 1, tolerance: 1 },
+  { color: 'red', value: 2, tolerance: 2 },
+  { color: 'orange', value: 3, tolerance: null },
+  { color: 'yellow', value: 4, tolerance: null },
+  { color: 'green', value: 5, tolerance: 0.5 },
+  { color: 'blue', value: 6, tolerance: 0.25 },
+  { color: 'violet', value: 7, tolerance: 0.1 },
+  { color: 'gray', value: 8, tolerance: 0.05 },
+  { color: 'white', value: 9, tolerance: null },
+  { color: 'gold', value: -1, tolerance: 5 },
+  { color: 'silver', value: -2, tolerance: 10 },
+  { color: '', value: null, tolerance: 20 }
 ]
 
-function Button(color: string) {
-  <button>{colorCodes[color].value}</button>
+interface ColorCodeProps extends ColorCodes {
+  bands: number
+  onClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
-function ColorRow() {
+function ColorRow({ bands, color, value, tolerance, onClick }: ColorCodeProps) {
   return (
     <div>
-      <p>Brown</p>
-      <button>C</button>
-      <button>D</button>
-      <button>E</button>
-      <button>F</button>
+      <span>{color}</span>
+      {[...Array(bands)].map((_, i) => {
+        if (value === null) return <button key={i} disabled>{''}</button>
+        else if (value < 0) return <button key={i} disabled>{''}</button>
+        return <button key={i} onClick={onClick} value={value} name={['firstDigit', 'secondDigit', 'thirdDigit'][i]}>{value}</button>
+      })}
+      {value === null ? '' : <button onClick={onClick} value={value} name="multiplier">10<sup>{value}</sup></button>}
+      {tolerance && <button onClick={onClick} value={tolerance} name="tolerance">&plusmn;{tolerance}%</button>}
+    </div>
+  )
+}
+
+function ColorCodesMap({ onClick }: {onClick: React.MouseEventHandler<HTMLButtonElement>}) {
+  const colorCodesMap = colorCodes.map(color => {
+    return <ColorRow bands={3} key={color.color} {...color} onClick={onClick} />
+  })
+  return (
+    <>
+      {colorCodesMap}
+    </>
+  )
+}
+
+interface Resistance {
+  firstDigit: number | null,
+  secondDigit: number | null,
+  thirdDigit: number | null,
+  multiplier: number | null,
+  tolerance: number | null
+  bands?: number
+}
+
+function OutputRow({ firstDigit, secondDigit, thirdDigit, multiplier, tolerance, bands }: Resistance) {
+  return (
+    <div>
+      {[...Array(bands)].map((_, i) => {
+        return <span key={i}>{[firstDigit, secondDigit, thirdDigit][i]}</span>
+      })}
+      {multiplier && <span> &times; 10<sup>{multiplier}</sup></span>}
+      {tolerance && <span>&plusmn;{tolerance}%</span>}
+      <span>Resistance</span>
+      <label>Select the unit to display the resistance</label>
+      <select name="resistance" defaultValue="default">
+        <option value="milli">m&#8486;</option>
+        <option value="default">&#8486;</option>
+        <option value="kilo">k&#8486;</option>
+        <option value="mega">M&#8486;</option>
+      </select>
     </div>
   )
 }
 
 export default function Resistance() {
+  const initialResistance = {
+    firstDigit: null,
+    secondDigit: null,
+    thirdDigit: null,
+    multiplier: null,
+    tolerance: null
+  }
+  const [resistance, setResistance] = useState({...initialResistance})
+
+  function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+    // clears the error: Property 'name' does not exist on type 'EventTarget'.
+    const target = e.target as HTMLButtonElement
+    setResistance({ ...resistance, [target.name]: target.value })
+  }
+
   return (
     <div>
       <p>Testing the Resistance calculator page!</p>
-      <ColorRow />
+      <ColorCodesMap onClick={handleClick} />
+      <OutputRow bands={3} {...resistance} />
     </div>
   )
 }
